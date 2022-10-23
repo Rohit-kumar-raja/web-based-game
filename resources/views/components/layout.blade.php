5fa84df158e6bf72d7659c0acb479539c4052a0b
Amit Prajapati <78385573+amitconnect18@users.noreply.github.com>
2022-10-23 12:18:14 +0530

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -39,7 +39,7 @@
 <script src="{{ asset('vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
 <script src="{{ asset('vendor/nouislider/distribute/nouislider.min.js') }}"></script>
 <script src="{{ asset('vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
-<script src="{{ asset('vendor/countup.js') }}/dist/countUp.umd.js') }}"></script>
+<script src="{{ asset('vendor/countup.js/dist/countUp.umd.js') }}"></script>
 <script src="{{ asset('vendor/apexcharts/dist/apexcharts.min.js') }}"></script>
 <script src="{{ asset('vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
 <script src="{{ asset('vendor/simple-datatables/dist/umd/simple-datatables.js') }}"></script>
@@ -48,7 +48,7 @@
 <script src="{{ asset('vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
 <script src="{{ asset('vendor/fullcalendar/main.min.js') }}"></script>
 <script src="{{ asset('vendor/dropzone/dist/min/dropzone.min.js') }}"></script>
-<script src="{{ asset('vendor/choices.js') }}/public/assets/scripts/choices.min.js') }}"></script>
+<script src="{{ asset('vendor/choices.js/assets/scripts/choices.min.js') }}"></script>
 <script src="{{ asset('vendor/notyf/notyf.min.js') }}"></script>
 <script src="{{ asset('vendor/leaflet/dist/leaflet.js') }}"></script>
 <script src="{{ asset('vendor/svg-pan-zoom/dist/svg-pan-zoom.min.js') }}"></script>
