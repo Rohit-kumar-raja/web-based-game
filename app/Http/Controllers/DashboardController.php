32c37a7f6881bb9794883e54e62a35c8e22f2820
Raja <raja@raja.com>
2022-12-23 00:09:47 +0530

    all doen

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AllUsers;
+use App\Models\Contest;
+use App\Models\Matches;
 use App\Models\Messages;
+use App\Models\Participated_user;
+use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Auth;
+
 class DashboardController extends Controller
 {
     function index()
     {
 
-        $from = date('2018-01-01');
-        $to = date('2018-05-02');
-  
+        $total_user = AllUsers::count();
+        $total_matches = Matches::count();
+        $total_contest = Contest::count();
+        $total_debit = Wallet::sum('debit');
+        $total_credit = Wallet::sum('credit');
+        $total_participate = Participated_user::count();
 
-        return view('dashboard');
+
+        return view('dashboard', [
+            'total_user' => $total_user,
+            'total_matches' => $total_matches,
+            'total_contest' => $total_contest,
+            'total_debit' => $total_debit,
+            'total_credit' => $total_credit,
+            'total_participate' => $total_participate,
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,29 +3,29 @@
          <div class="py-4">
              {{-- <div class="row justify-content-lg-center">
                  <div class="col-12 mb-4">
-                     <div class="card border-0 bg-yellow-100 shadow">
+                     <div class="card mb-3 border-0 bg-yellow-100 shadow">
                          <div class="card-header d-sm-flex flex-row align-items-center border-yellow-200 flex-0">
                              <div class="d-block mb-3 mb-sm-0">
                                  <div class="fs-5 fw-normal mb-2"> <i class="fas fa-comment-alt-exclamation    "></i> Total
                                      Messages</div> --}}
-                                 {{-- <h2 class="fs-3 fw-extrabold">{{ $total_message }}</h2>  --}}
-                                 {{-- <div class="small mt-2"><span class="fw-normal me-2">Yesterday</span> <span
+             {{-- <h2 class="fs-3 fw-extrabold">{{ $total_message }}</h2>  --}}
+             {{-- <div class="small mt-2"><span class="fw-normal me-2">Yesterday</span> <span
                                          class="fas fa-angle-up text-success"></span>
                                      <span class="text-success fw-bold">10.57%</span>
                                  </div> --}}
-                             </div>
-                         </div>
-                         <div class="card-body p-2">
-                             {{-- <div id="chart"></div> --}}
-                         </div>
-                     {{-- </div>
+         </div>
+         </div>
+         <div class="card-body p-2">
+             {{-- <div id="chart"></div> --}}
+         </div>
+         {{-- </div>
                  </div>
              </div> --}}
 
-             {{-- norification section --}}
-             {{-- <div class="row">
+         {{-- norification section --}}
+         {{-- <div class="row">
                  <div class="col-12 col-md-12 col-xxl-4 mb-4">
-                     <div class="card notification-card border-0 shadow">
+                     <div class="card mb-3 notification-card mb-3 border-0 shadow">
                          <div class="card-header d-flex align-items-center">
                              <h2 class="fs-5 fw-bold mb-0">Notifications</h2>
                              <div class="ms-auto"><a class="fw-normal d-inline-flex align-items-center"
@@ -63,6 +63,193 @@
                      </div>
                  </div>
              </div> --}}
-             {{-- notification section end --}}
-         @endslot
+         {{-- notification section end --}}
+         <div class="container-fluid">
+             <div class="row">
+                 <div class="col-12 col-lg-6 col-xl">
+
+                     <!-- Value  -->
+                     <div class="card mb-3 bg-info">
+                         <div class="card-body">
+                             <div class="row align-items-center gx-0">
+                                 <div class="col">
+
+                                     <!-- Title -->
+                                     <h6 class="text-uppercase text-muted mb-2 text-white">
+                                         Total User's
+                                     </h6>
+
+                                     <!-- Heading -->
+                                     <span class="h2 mb-0 text-white">
+                                         {{ $total_user }} +
+                                     </span>
+
+
+                                 </div>
+                                 <div class="col-auto">
+
+                                     <!-- Icon -->
+                                     <i class="fas fa-users font-45"></i>
+
+                                 </div>
+                             </div> <!-- / .row -->
+                         </div>
+                     </div>
+
+                 </div>
+                 <div class="col-12 col-lg-6 col-xl">
+
+                     <!-- Value  -->
+                     <div class="card mb-3 bg-warning">
+                         <div class="card-body">
+                             <div class="row align-items-center gx-0">
+                                 <div class="col">
+
+                                     <!-- Title -->
+                                     <h6 class="text-uppercase text-muted mb-2 text-white">
+                                         Total Mathe's
+                                     </h6>
+
+                                     <!-- Heading -->
+                                     <span class="h2 mb-0 text-white">
+                                         {{ $total_matches }} +
+                                     </span>
+
+
+                                 </div>
+                                 <div class="col-auto">
+
+                                     <!-- Icon -->
+                                     <i class="fas fa-bolt font-45"></i>
+
+                                 </div>
+                             </div> <!-- / .row -->
+                         </div>
+                     </div>
+
+                 </div>
+
+                 <div class="col-12 col-lg-6 col-xl">
+
+                     <!-- Value  -->
+                     <div class="card mb-3 bg-danger">
+                         <div class="card-body">
+                             <div class="row align-items-center gx-0">
+                                 <div class="col">
+
+                                     <!-- Title -->
+                                     <h6 class="text-uppercase text-muted mb-2 text-white">
+                                         Total Contest
+                                     </h6>
+
+                                     <!-- Heading -->
+                                     <span class="h2 mb-0 text-white">
+                                         {{ $total_contest }} +
+                                     </span>
+
+
+                                 </div>
+                                 <div class="col-auto">
+                                     <i class="fas fa-equals font-45"></i>
+
+                                     <!-- Icon -->
+                                 </div>
+                             </div> <!-- / .row -->
+                         </div>
+                     </div>
+
+                 </div>
+                 <div class="col-12 col-lg-6 col-xl">
+
+                     <!-- Value  -->
+                     <div class="card mb-3 bg-primary">
+                         <div class="card-body">
+                             <div class="row align-items-center gx-0">
+                                 <div class="col">
+
+                                     <!-- Title -->
+                                     <h6 class="text-uppercase text-muted mb-2 text-white">
+                                          Participated 
+                                     </h6>
+
+                                     <!-- Heading -->
+                                     <span class="h2 mb-0 text-white">
+                                         {{ $total_participate }} +
+                                     </span>
+
+
+                                 </div>
+                                 <div class="col-auto">
+
+                                     <!-- Icon -->
+                                     <i class="fas fa-users font-45"></i>
+                                 </div>
+                             </div> <!-- / .row -->
+                         </div>
+                     </div>
+
+                 </div>
+                 <div class="col-12 col-lg-6 col-xl">
+
+                    <!-- Value  -->
+                    <div class="card mb-3 bg-warning">
+                        <div class="card-body">
+                            <div class="row align-items-center gx-0">
+                                <div class="col">
+
+                                    <!-- Title -->
+                                    <h6 class="text-uppercase text-muted mb-2 text-white">
+                                        Total Debit
+                                    </h6>
+
+                                    <!-- Heading -->
+                                    <span class="h2 mb-0 text-white">
+                                        {{ $total_debit }} +
+                                    </span>
+
+
+                                </div>
+                                <div class="col-auto">
+
+                                    <!-- Icon -->
+                                    <i class="fas fa-rupee-sign font-45"></i>
+                                </div>
+                            </div> <!-- / .row -->
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-12 col-lg-6 col-xl">
+
+                    <!-- Value  -->
+                    <div class="card mb-3 bg-success">
+                        <div class="card-body">
+                            <div class="row align-items-center gx-0">
+                                <div class="col">
+
+                                    <!-- Title -->
+                                    <h6 class="text-uppercase text-muted mb-2 text-white">
+                                        Total Credit
+                                    </h6>
+
+                                    <!-- Heading -->
+                                    <span class="h2 mb-0 text-white">
+                                        {{ $total_credit }} +
+                                    </span>
+
+
+                                </div>
+                                <div class="col-auto">
+
+                                    <!-- Icon -->
+                                    <i class="fas fa-rupee-sign font-45"></i>
+                                </div>
+                            </div> <!-- / .row -->
+                        </div>
+                    </div>
+
+                </div>
+             </div> <!-- / .row -->
+         </div>
+     @endslot
  </x-layout>
